7e25b34403ff3bd95cdc7c8f80ff5ee86d9e4fa9
Your Name <you@example.com>
2021-08-09 22:36:27 +0900

    level2 if문 - 알람시계 2884번 (풀이완료)

--- BackJun/src/level2/alarm_clock_2884.java
+++ BackJun/src/level2/alarm_clock_2884.java
@@ -6,27 +6,23 @@ public class alarm_clock_2884 {
 	public static void main(String[] args) {
 		int h, m;
 		Scanner sc = new Scanner(System.in);
-		
+
 		h = sc.nextInt();
 		m = sc.nextInt();
-		
-		sc.close();
-		
-		if(h > 0) {
-			if(m < 45) {
-				System.out.println((h-1) + " " + ((m-45)+60));
 
-			} else if(m > 45){
-				System.out.println(h + " " + (m-45));
-			
+		sc.close();
+
+		if (h == 0) {
+			if (m >= 45) {
+				System.out.println(h + " " + (m - 45));
+			} else {
+				System.out.println((h + 23) + " " + (m + 15));
 			}
 		} else {
-			if(m < 45) {
-				System.out.println((h+23) + " " + ((m-45)+60));
-				
-			} else if(m > 45) {
-				System.out.println((h+23) + " " + (m-45));
-				
+			if (m >= 45) {
+				System.out.println(h + " " + (m - 45));
+			} else {
+				System.out.println((h - 1) + " " + (m + 15));
 			}
 		}
 	}
